e00912798d8c9f0a18bb520ecc6b34d9e01a0e1b
YesidGuerrero <111210918+YesidGuerrero@users.noreply.github.com>
2024-04-26 11:51:32 -0500

    Codigos

--- CodigoIGAC.py
+++ CodigoIGAC.py
@@ -4,24 +4,24 @@ import csv
 import gnsscal
 
 #Leemos un archivo con las 105 estaciones SGC, las enlistamos y convertimos los caracteres a minusculas
-EstacionesSGC=[' ',' ','ESTACIÓN']
+EstacionesIGAC=[' ',' ','ESTACIÓN']
 Estacionescomp=[]
 
-with open('EstacionesSGC.csv', newline='') as File:
+with open('EstacionesIGAC.csv', newline='') as File:
     reader=csv.reader(File)
     for fila in reader:
-        EstacionesSGC.append(fila[0])
+        EstacionesIGAC.append(fila[0])
         Estacionescomp.append(fila[0])
-        EstacionesSGCmin=[elemento.lower() for elemento in Estacionescomp]
+        EstacionesIGACmin=[elemento.lower() for elemento in Estacionescomp]
 
 #Creamos un dataframe
-tabla = pd.DataFrame(EstacionesSGC)
-#print(len(EstacionesSGC))
-#print(len(EstacionesSGCmin))
+tabla = pd.DataFrame(EstacionesIGAC)
+#print(len(EstacionesIGAC))
+#print(len(EstacionesIGACmin))
 
 
 #Ingresamos el directorio
-directorio='/Users/hp/Documents/Documento/IGAC/Depuración/1'
+directorio='//172.26.0.20/Elite_Sub_Geografia_Cartografia/3130GITGeodesia/90RSMNReferencia/3EOContinua/9ARINEX/Rinex15s/2024'
 
 #definimos una funcion que determina a que semana GPS pertenece el doy
 def doy2GPSWeek(año,doy):
@@ -35,20 +35,20 @@ def doy2GPSWeek(año,doy):
 #Recorremos todas las carpetas y hacemos un listado de todas las estaciones
 with os.scandir(directorio) as ficheros:
     for fichero in ficheros:
-
+        print(fichero.name)
         #Creamos las 3 primeras filas del archivo excel que contiene vacio, semana gnss y dia gnss
         ActividadDiaria=['']
         GPSweek,DayWeek=doy2GPSWeek(2024,int(fichero.name))
         ActividadDiaria.append(GPSweek)
         ActividadDiaria.append(fichero.name)
-
+	
+	
         #Se crea una nueva ruta direc agregandole a directorio el nombre de cada fichero
-        direc= os.path.join(directorio,fichero.name)
+        direc= os.path.join(directorio,fichero.name, '24o/V2.11')
+        
 
-
-
-#        #Se recorre la carpeta con ruta direc, comparamos los archivos existentes con EstacionesSGC
- #       for elemento in EstacionesSGCmin:
+#        #Se recorre la carpeta con ruta direc, comparamos los archivos existentes con EstacionesIGAC
+ #       for elemento in EstacionesIGACmin:
  #           #print("estacion:", elemento)
  #           #print(fichero.name)
  #           #print(ActividadDiaria)
@@ -63,15 +63,14 @@ with os.scandir(directorio) as ficheros:
 #                    else:
 #                        ActividadDiaria.append('0')
 #                        break
-        lon=len(EstacionesSGCmin)
+        lon=len(EstacionesIGACmin)
         for i in range(0,lon):
             n=0
             for file in os.listdir(direc):
                     file_lower=file.lower()
-                    #print(file_lower,"-",EstacionesSGCmin[i])
-                    if file_lower.startswith(EstacionesSGCmin[i]):
-                        n=n+1
-                    #print("n:",n)
+                    #print(file_lower,"-",EstacionesIGACmin[i])
+                    if file_lower.startswith(EstacionesIGACmin[i]):
+                        n=n+1                    
             if n!=0:
                 ActividadDiaria.append('1')
             else:
@@ -80,5 +79,4 @@ with os.scandir(directorio) as ficheros:
         #print(tabla)
         #print(len(ActividadDiaria))
 
-tabla.to_csv("Datos.csv", index=False)
-print(tabla)
+tabla.to_csv("DatosIGAC.csv", index=False)
---
+++ CodigoSGC.py
@@ -0,0 +1,82 @@
+import os
+import pandas as pd
+import csv
+import gnsscal
+
+#Leemos un archivo con las 105 estaciones SGC, las enlistamos y convertimos los caracteres a minusculas
+EstacionesSGC=[' ',' ','ESTACIÓN']
+Estacionescomp=[]
+
+with open('EstacionesSGC.csv', newline='') as File:
+    reader=csv.reader(File)
+    for fila in reader:
+        EstacionesSGC.append(fila[0])
+        Estacionescomp.append(fila[0])
+        EstacionesSGCmin=[elemento.lower() for elemento in Estacionescomp]
+
+#Creamos un dataframe
+tabla = pd.DataFrame(EstacionesSGC)
+#print(len(EstacionesSGC))
+#print(len(EstacionesSGCmin))
+
+
+#Ingresamos el directorio
+directorio='//172.26.0.20/Elite_Sub_Geografia_Cartografia/3130GITGeodesia/90RSMNReferencia/3EOContinua/9ARINEX/Rinex_15s_SGC/OBS/2024'
+
+#definimos una funcion que determina a que semana GPS pertenece el doy
+def doy2GPSWeek(año,doy):
+    fecha=gnsscal.yrdoy2date(año, doy)
+    GPSWeek=gnsscal.date2gpswd(fecha)
+    return GPSWeek
+
+
+
+
+#Recorremos todas las carpetas y hacemos un listado de todas las estaciones
+with os.scandir(directorio) as ficheros:
+    for fichero in ficheros:
+        print(fichero.name)
+        #Creamos las 3 primeras filas del archivo excel que contiene vacio, semana gnss y dia gnss
+        ActividadDiaria=['']
+        GPSweek,DayWeek=doy2GPSWeek(2024,int(fichero.name))
+        ActividadDiaria.append(GPSweek)
+        ActividadDiaria.append(fichero.name)
+
+        #Se crea una nueva ruta direc agregandole a directorio el nombre de cada fichero
+        direc= os.path.join(directorio,fichero.name)
+
+
+
+#        #Se recorre la carpeta con ruta direc, comparamos los archivos existentes con EstacionesSGC
+ #       for elemento in EstacionesSGCmin:
+ #           #print("estacion:", elemento)
+ #           #print(fichero.name)
+ #           #print(ActividadDiaria)
+#
+#            with os.scandir(direc) as files:
+#                for file in files:
+#                    file_lower=file.name.lower()
+#
+#                    if file_lower.startswith(elemento):
+#                        ActividadDiaria.append('1')
+#                        break
+#                    else:
+#                        ActividadDiaria.append('0')
+#                        break
+        lon=len(EstacionesSGCmin)
+        for i in range(0,lon):
+            n=0
+            for file in os.listdir(direc):
+                    file_lower=file.lower()
+                    #print(file_lower,"-",EstacionesSGCmin[i])
+                    if file_lower.startswith(EstacionesSGCmin[i]):
+                        n=n+1                    
+            if n!=0:
+                ActividadDiaria.append('1')
+            else:
+                ActividadDiaria.append('0')
+        tabla[fichero.name]=ActividadDiaria
+        #print(tabla)
+        #print(len(ActividadDiaria))
+
+tabla.to_csv("Datos.csv", index=False)
