7439b25ae502d1b888cc9316d8d8e64d6b059085
YesidGuerrero <111210918+YesidGuerrero@users.noreply.github.com>
2024-05-02 13:52:01 -0500

    .

--- CodigoSGC.py
+++ CodigoSGC.py
@@ -3,6 +3,11 @@ import pandas as pd
 import csv
 import gnsscal
 
+
+#Ingresamos el directorio
+directorio='//172.26.0.20/Elite_Sub_Geografia_Cartografia/3130GITGeodesia/90RSMNReferencia/3EOContinua/9ARINEX/Rinex_15s_SGC/OBS/2024'
+
+
 #Leemos un archivo con las 105 estaciones SGC, las enlistamos y convertimos los caracteres a minusculas
 EstacionesSGC=[' ',' ','ESTACIÓN']
 Estacionescomp=[]
@@ -14,22 +19,52 @@ with open('EstacionesSGC.csv', newline='') as File:
         Estacionescomp.append(fila[0])
         EstacionesSGCmin=[elemento.lower() for elemento in Estacionescomp]
 
-#Creamos un dataframe
-tabla = pd.DataFrame(EstacionesSGC)
-#print(len(EstacionesSGC))
-#print(len(EstacionesSGCmin))
 
 
-#Ingresamos el directorio
-directorio='//172.26.0.20/Elite_Sub_Geografia_Cartografia/3130GITGeodesia/90RSMNReferencia/3EOContinua/9ARINEX/Rinex_15s_SGC/OBS/2024'
+#Creamos una clase para los dataframes correspondientes a cada mes
+class DataFrame:
+    def __init__(self, data_dict):
+        self.data=pd.DataFrame(data_dict)
+
+    def mostrar_dataframe(self):
+        print(self.data)
+
+
+
+
 
 #definimos una funcion que determina a que semana GPS pertenece el doy
 def doy2GPSWeek(año,doy):
     fecha=gnsscal.yrdoy2date(año, doy)
     GPSWeek=gnsscal.date2gpswd(fecha)
+    mes=fecha.strftime('%m')
     return GPSWeek
 
 
+
+#Creacion de array dataframes para meses disponibles hasta el momento funcion que genera los dataframe para cada mes
+Meses=[]
+def MesesDisponibles():
+    with os.scandir(directorio) as ficheros:
+        for fichero in ficheros:
+            n=0
+            fecha=gnsscal.yrdoy2date(2024, int(fichero.name))
+            mont=fecha.strftime('%m')
+            for mes in Meses:
+                if mont == mes:
+                    n=n+1
+            if n==0:
+                Meses.append(mont)
+    return
+
+MesesDisponibles()
+
+#Creamos los dataframe para cada mes
+print(Meses)
+
+"""
+#Creamos un dataframe
+tabla = pd.DataFrame(EstacionesSGC)
 
 
 #Recorremos todas las carpetas y hacemos un listado de todas las estaciones
@@ -46,23 +81,6 @@ with os.scandir(directorio) as ficheros:
         direc= os.path.join(directorio,fichero.name)
 
 
-
-#        #Se recorre la carpeta con ruta direc, comparamos los archivos existentes con EstacionesSGC
- #       for elemento in EstacionesSGCmin:
- #           #print("estacion:", elemento)
- #           #print(fichero.name)
- #           #print(ActividadDiaria)
-#
-#            with os.scandir(direc) as files:
-#                for file in files:
-#                    file_lower=file.name.lower()
-#
-#                    if file_lower.startswith(elemento):
-#                        ActividadDiaria.append('1')
-#                        break
-#                    else:
-#                        ActividadDiaria.append('0')
-#                        break
         lon=len(EstacionesSGCmin)
         for i in range(0,lon):
             n=0
@@ -80,3 +98,4 @@ with os.scandir(directorio) as ficheros:
         #print(len(ActividadDiaria))
 
 tabla.to_csv("Datos.csv", index=False)
+"""
